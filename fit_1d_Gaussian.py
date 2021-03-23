3002fd561a92ba1464c69b6b6d67cbced4448f57
JoschaJ <jjahns@mpifr-bonn.mpg.de>
2021-03-23 19:58:02 +0100

    Fixed all bugs; added a plotter.

--- fit_1d_Gaussian.py
+++ fit_1d_Gaussian.py
@@ -5,11 +5,14 @@ Created on Tue Mar  9 13:22:47 2021
 
 @author: jjahns
 """
+import os
 import argparse
 import pickle
 import numpy as np
 import pandas as pd
 import warnings
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
 
 import import_fil_fits
 
@@ -42,7 +45,7 @@ def fit_multiple_1d_gaussians(x_data, y_data, amp_guess, x_guess, std_guess, wei
 # =============================================================================
 
 def fit_time_and_freq(waterfall, tsamp, freqs, offpulsefile, amp_guess, t_guess, t_std_g, f_guess,
-                      f_std_g, t_fit, downs):
+                      f_std_g, t_fit, downs, make_plot=False):
     """
     waterfall : waterfall bandpass corrected to SNR
     """
@@ -68,14 +71,13 @@ def fit_time_and_freq(waterfall, tsamp, freqs, offpulsefile, amp_guess, t_guess,
 
     times = np.arange(time_series.shape[0]) * tsamp
     time_series = time_series[window_start:widow_end]
-    times = times[window_start:widow_end]
 
     # Convert t_guess to time units and lower amp_guess
-    t_guess *= tsamp
+    t_guess = t_guess * tsamp
     amp_guess = amp_guess / off_std / 2  # assume a pulse goes over half the band
 
-    gaussians, fitter = fit_multiple_1d_gaussians(times, time_series, amp_guess,
-                                                  t_guess, t_std_g)
+    gaussians, fitter = fit_multiple_1d_gaussians(times[window_start:widow_end], time_series,
+                                                  amp_guess, t_guess, t_std_g)
 
     gauss_params = gaussians.parameters.reshape((gaussians.n_submodels, 3))
     cov_mat = fitter.fit_info['param_cov']
@@ -92,6 +94,12 @@ def fit_time_and_freq(waterfall, tsamp, freqs, offpulsefile, amp_guess, t_guess,
     f_std = np.zeros(f_guess.shape)
     f_cent_e = np.zeros(f_guess.shape)
     f_std_e = np.zeros(f_guess.shape)
+
+    # Make gaussians iterable in case there is only one component.
+    try:
+        iter(gaussians)
+    except TypeError:
+        gaussians = (gaussians,)
     # Fit each subburst in frequency.
     for i, gauss in enumerate(gaussians):
         # Use only the inner 2 sigma in time.
@@ -100,29 +108,37 @@ def fit_time_and_freq(waterfall, tsamp, freqs, offpulsefile, amp_guess, t_guess,
 
         # Fit a Gaussian in frequency.
         weights = (~sb_spectrum.mask).astype(np.int)
-        freq_gauss, freq_fitter = fit_multiple_1d_gaussians(freqs, sb_spectrum.data, amp_guess[i],
-                                                  f_guess[i], f_std_g[i], weights=weights)
+        freq_gauss, freq_fitter = fit_multiple_1d_gaussians(freqs, sb_spectrum.data, amp_guess[i:i+1],
+                                                  f_guess[i:i+1], f_std_g[i:i+1], weights=weights)
 
         # Save the fitted parameters.
-        f_cent[i], f_std[i] = freq_gauss.mean, freq_gauss.stddev
+        f_cent[i], f_std[i] = freq_gauss.mean.value, freq_gauss.stddev.value
         try:
             errors = np.sqrt(np.diag(freq_fitter.fit_info['param_cov']))
             _, f_cent_e[i], f_std_e[i] = errors
         except ValueError:
-            print("The frequency fit did not work. {freq_fitter.fit_info['message']}")
+            print(f"The frequency fit did not work. {freq_fitter.fit_info['message']}")
             f_cent_e[i], f_std_e[i] = np.nan, np.nan
 
+    if make_plot:
+        plot_fits(waterfall, times, freqs, time_series, sb_spectrum, gaussians, freq_gauss, offpulse)
+
     amps, t_cent, t_std = gauss_params[:, 0], gauss_params[:, 1], gauss_params[:, 2]
-    amps_e, t_cent_e, t_std_e = uncert
+    amps_e, t_cent_e, t_std_e = uncert[0::3], uncert[1::3], uncert[2::3]
     return amps, amps_e, t_cent, t_cent_e, t_std, t_std_e, f_cent, f_cent_e, f_std, f_std_e
 
 
-def fit_observation(basename, orig_hdf5_file=None, propery_hdf5=None, pulse=None, tavg=1, subb=1):
+def fit_observation(basename, orig_hdf5_file=None, propery_hdf5=None, pulse=None, tavg=1, subb=1,
+                    make_plot=False):
+    warnings.filterwarnings("ignore", message="Polarization is AABBCRCI, averaging AA and BB")
     # hdf5 file
     if not orig_hdf5_file:
         orig_hdf5_file = f'{basename}.hdf5'
     if not propery_hdf5:
         property_hdf5 = f'{basename}_burst_properties.hdf5'
+
+    if not os.path.isfile(orig_hdf5_file) and os.path.isdir(basename):
+        os.chdir(os.path.join(basename, 'pulses'))
 
     pulses = pd.read_hdf(property_hdf5, 'pulses')
 
@@ -138,6 +154,7 @@ def fit_observation(basename, orig_hdf5_file=None, propery_hdf5=None, pulse=None
     for pulse_id in pulse_ids:
         print("1D Gaussian fit of observation %s, pulse ID %s" % (basename, pulse_id))
         base_pulse = pulse_id.split('-')[0]
+        global pulse_dir
         pulse_dir = f'{base_pulse}/'
         fits_file = pulse_dir + f'{basename}_{base_pulse}.fits'
 
@@ -168,7 +185,7 @@ def fit_observation(basename, orig_hdf5_file=None, propery_hdf5=None, pulse=None
                                                          AO=True, smooth_val=None, plot=False)
 
         fit_results = fit_time_and_freq(waterfall, tsamp, freqs, offpulsefile, amp_guesses, time_guesses, t_std_guess,
-                          freq_peak_guess, f_std_guess, t_fit, tavg)
+                          freq_peak_guess, f_std_guess, t_fit, tavg, make_plot=make_plot)
 
         # Save all.
         pulses.loc[pulse_id, ('Gaussian', 'Amp')] = fit_results[0]
@@ -187,18 +204,46 @@ def fit_observation(basename, orig_hdf5_file=None, propery_hdf5=None, pulse=None
             pulses.to_hdf(property_hdf5, 'pulses')
 
 
+def plot_fits(waterfall, times, freqs, time_series, sb_spectrum, gaussians, freq_gauss, offpulse):
+    rows = 2
+    cols = 2
+    gs = gridspec.GridSpec(rows, cols, wspace=0., hspace=0.,
+                           height_ratios=[0.5,]*(rows-1) + [2,],
+                           width_ratios=[5,] + [1,]*(cols-1))
+    fig = plt.figure('1D fit', figsize=(16, 10))
+    ax_data = plt.subplot(gs[2])
+    ax_ts = plt.subplot(gs[0], sharex=ax_data)
+    ax_spec = plt.subplot(gs[-1], sharey=ax_data)
+    ax_data.imshow(waterfall, origin='upper', aspect='auto', extent=[times.min(), times.max(), freqs[-1], freqs[0]])
+
+    time_series = waterfall.mean(axis=0)
+    off_mean = time_series[offpulse].mean()
+    off_std = time_series[offpulse].std()
+    time_series = (time_series - off_mean) / off_std
+
+    if isinstance(gaussians, tuple):
+        gaussians = gaussians[0]
+    ax_ts.plot(times, time_series)
+    ax_ts.plot(times, gaussians(times))
+    ax_spec.plot(sb_spectrum, freqs)
+    ax_spec.plot(freq_gauss(freqs), freqs)
+
+    fig.savefig(pulse_dir+'1D_fit.png')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('basename',
                         help='The base of the observation (e.g. puppi_58564_C0531+33_0020)')
     parser.add_argument('-p', '--pulse', type=str, help="Give a pulse id to process only this pulse.")
-    parser.add_argument('-t', '--tavg', dest='tavg', type='int', default=1,
+    parser.add_argument('-t', '--tavg', dest='tavg', type=int, default=1,
                       help="If -t option is used, time averaging is applied using the factor "
                            "given after -t.")
-    parser.add_argument('-s', '--subb', type='int', default=1,
+    parser.add_argument('-s', '--subb', type=int, default=1,
                       help="If -s option is used, subbanding is applied using the factor "
                            "given after -s.")
+    parser.add_argument('-m', '--make_plot', type=bool, default=False,
+                      help="Make a waterfaller plot with the fitted functions.")
 
     args = parser.parse_args()
 
